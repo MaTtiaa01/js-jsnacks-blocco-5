dee17316156dceb17c647f17c8b90644c9f74c61
MaTtiaa01 <mattia2001@live.it>
2022-10-24 18:40:42 +0200

    fix js-snack-5

--- js-snack-5/main.js
+++ js-snack-5/main.js
@@ -7,18 +7,32 @@
 const normalArr = [3,4,5,6,7,8,9,10];
 const numb1 = 4;
 const numb2 = 9;
+const finalArr =[];
 
-function pickNumb(array,numb1,numb2) {
-    
-    const newArr = [];
-    array.forEach((element) => {
-        console.log(element);
-        if (element > numb1 && element < numb2) {
-            newArr.push(element)
-        }
-    });
-    return newArr
+
+function filterNumbers(min, max) {
+    return function (a) { return a >= min && a <= max; };
 }
 
-const finalArr = pickNumb(normalArr,numb1,numb2);
+finalArr.push(normalArr.filter(filterNumbers(numb1,numb2)));
 console.log(finalArr);
+
+
+
+
+
+
+// function pickNumb(array,numb1,numb2) {
+    
+//     const newArr = [];
+//     array.forEach((element) => {
+//         console.log(element);
+//         if (element > numb1 && element < numb2) {
+//             newArr.push(element)
+//         }
+//     });
+//     return newArr
+// }
+
+// const finalArr = pickNumb(normalArr,numb1,numb2);
+// console.log(finalArr);
