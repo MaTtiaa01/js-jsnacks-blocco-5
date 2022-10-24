db750ffc75844ef45e68c08626deb9b338264c35
MaTtiaa01 <mattia2001@live.it>
2022-10-24 15:58:25 +0200

    add js-snack-4

---
+++ js-snack-4/main.js
@@ -0,0 +1,20 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di
+//     elementi) prendendo alternativamente gli elementi da uno e dall’altro
+//     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+const firstArr = ["a","b","c"];
+const secondArr = [1,2,3];
+
+//do a forEach in both of array to select each element
+//create a empty array
+// through a for, push every element using firstArr[i] and secondArr[i]
+
+const mixArr = [];
+
+for (let i = 0; i < firstArr.length; i++) {
+    mixArr.push(firstArr[i],secondArr[i])
+    
+}
+console.log(mixArr);
+
